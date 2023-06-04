be0c521ddd41f7dead5146761258a661d9332518
DirkEz <dirk.schaafstra@gmail.com>
2023-06-05 01:02:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'layout/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="icon" type="image/x-icon" href="img\icon.png">     -->
+    <title>016 | Home</title>
+</head>
+<body>
+    
+</body>
+</html>
