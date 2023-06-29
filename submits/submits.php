d00ba68398578bce5eb1691d9fb6c79798a78184
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:50 +0200

    Update submits.php

--- submits/submits.php
+++ submits/submits.php
@@ -21,7 +21,7 @@ $stmt = $connect->query("SELECT * FROM submits WHERE view=0");
 		<meta charset="utf-8">
 		<title>Welcome, <?=$_SESSION['name']?>!</title>
         <link rel="stylesheet" href="css/style.css">
-		<link href="css\user.css" rel="stylesheet" type="text/css">
+		<link href="css/user.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
@@ -35,7 +35,7 @@ $stmt = $connect->query("SELECT * FROM submits WHERE view=0");
                 ?>
 				<a href="../user.php"><i class="fa-solid fa-user"></i> User page</a>
 				<a href="user/profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="../user\logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+				<a href="../user/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<?php 
