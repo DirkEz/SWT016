1681b4f053a660767cbf5c7ef47c139c182af9b3
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:55 +0200

    Update register-redirect.php

--- user-creation/register-redirect.php
+++ user-creation/register-redirect.php
@@ -2,11 +2,12 @@
     require_once('config.php');
     
     if (isset($_POST['register'])){
+        $wachtwoord = password_hash($_POST['password'], PASSWORD_DEFAULT);
         $sql = "INSERT INTO accounts (username, password, email)
         VALUES (:username,:password, :email)";
         $stmt = $connect->prepare($sql);
         $stmt->bindParam(":username", $_POST['username']);
-        $stmt->bindParam(":password", $_POST['password']);
+        $stmt->bindParam(":password", $wachtwoord);
         $stmt->bindParam(":email", $_POST['email']);
         $stmt->execute();
         $result = $stmt->fetch();
