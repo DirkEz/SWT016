51beb6debf313be6555d8db07c45eb715d62e769
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:44:00 +0200

    Update profile.php

--- user/profile.php
+++ user/profile.php
@@ -24,7 +24,7 @@ $stmt->close();
 		<meta charset="utf-8">
 		<title>Profile Page</title>
 		<link rel="stylesheet" href="../css/style.css">
-		<link href="..\css\user.css" rel="stylesheet" type="text/css">
+		<link href="../css/user.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
