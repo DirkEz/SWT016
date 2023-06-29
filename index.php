6c814bf19585d6a6d1fb48eb3e99fcb8cb9830bf
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?php
+<?php 
 
-include_once 'layout/header.php';
+session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -9,11 +10,33 @@ include_once 'layout/header.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="icon" type="image/x-icon" href="img\icon.png">     -->
+    <link rel="stylesheet" href="css/home.css">
     <title>016 | Home</title>
 </head>
 <body>
-    
+    <div class="panels">
+        <div class="panels__container">
+            <a href="submit.php" class="panel">
+                <div class="panel__content" style="background-image: url(https://cdn.discordapp.com/attachments/1115057844048375929/1117930629418459166/25F985FE-ED40-418A-8B35-0ED1F9AB1B3E.png);">
+                    <h3 class="panel__title">SUBMIT</h3>
+                </div>
+            </a>
+            <a href="login.php" class="panel">
+                <div class="panel__content" style="background-image: url(https://cdn.discordapp.com/attachments/1115057844048375929/1117930630366376138/E6D832B2-0881-4A5C-85DD-6FEE2AD520F7.png)">
+                    <h3 class="panel__title">    
+                    <?php   
+                        if (!isset($_SESSION['loggedin'])){
+                            echo "Login";
+                        } else {
+                            echo $_SESSION['name'];
+                        }
+                    ?>
+                    </h3>
+                </div>
+            </a>
+        </div>
+    </div>  
+    <!-- Made by Dirk Schaafstra https://dirkschaafstra.nl -->
 </body>
 </html>
+
