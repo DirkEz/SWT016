aebcc9cfe769063b88feeede9a97da41b897b682
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:36 +0200

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -5,16 +5,14 @@ session_start();
 
 <!DOCTYPE html>
 <!-- <link rel="stylesheet" href="../css/style.css"> -->
-<link rel="icon" type="image/x-icon" href="img\icon.png">
+<link rel="icon" type="image/x-icon" href="img/icon.png">
 <header class="site-header">
       <div class="wrapper site-header__wrapper">
         <a href="index.php" class="brand">016</a>
         <nav class="nav">
           <ul class="nav__wrapper">
-            <ul class="nav__item"><a href="#">Home</a></ul>
-            <ul class="nav__item"><a href="#">Info</a></ul>
+            <ul class="nav__item"><a href="index.php">Home</a></ul>
             <ul class="nav__item"><a href="submit.php">Submit</a></ul>
-            <ul class="nav__item"><a href="#">Contact</a></ul>
             <ul class="nav__item"><button type="submit" ><a href="./login.php">
 
             <?php 
