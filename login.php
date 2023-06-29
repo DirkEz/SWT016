29d9bfd5a38cf3a7a606ed74420946a7c123fec2
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
 include_once 'layout/header.php';
-// include_once 'config\config.php';
+// include_once 'config/config.php';
 ?> <p class="loggedin"> <?php if ($_SESSION['loggedin'] == TRUE ){
 	if ($_SESSION['is_admin'] === 1){
 		echo 'Welcome admin, ' . $_SESSION['name'] . '!';
@@ -16,14 +16,14 @@ include_once 'layout/header.php';
 	<head>
 		<meta charset="utf-8">
 		<title>Login</title>
-		<link rel="stylesheet" href="css\style.css">
-        <link rel="stylesheet" href="css\login.css">
+		<link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/login.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body>
 		<div class="login">
 			<h1>Login</h1>
-			<form action="config\authenticate.php" method="post">
+			<form action="config/authenticate.php" method="post">
 				<label for="username">
 					<i class="fas fa-user"></i>
 				</label>
