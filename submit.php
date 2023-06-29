f41905789123777b18e7f4f61011632fe5e98a0e
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:42 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,17 +1,23 @@
 <?php
 
+
 include_once 'layout/header.php';
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: login.php');
 	exit;
 }
-include_once 'user-creation\config.php';
+include_once 'user-creation/config.php';
+
+$name = $_SESSION['name'];
+
+$stmt2 = $connect->query("SELECT id FROM accounts WHERE username='$name'");
+$id = $_SESSION['id'];
 
 if(isset($_POST['submitID'])){
     
 
-    $sql = "INSERT INTO submits(email, artists, public_title, discord, genre, bpm, instagram, link, comment)
-    VALUES (:submit_email, :submit_artists, :submit_publicname, :submit_discord, :submit_genre, :submit_bpm, :submit_insta, :submit_link, :submit_comment)";
+    $sql = "INSERT INTO submits(email, artists, public_title, discord, genre, bpm, instagram, link, comment, userid)
+    VALUES (:submit_email, :submit_artists, :submit_publicname, :submit_discord, :submit_genre, :submit_bpm, :submit_insta, :submit_link, :submit_comment, :userid)";
     $stmt = $connect->prepare($sql);
     $stmt->bindParam(":submit_email", $_POST['submit_email']);
     $stmt->bindParam(":submit_artists", $_POST['submit_artists']);
@@ -22,9 +28,10 @@ if(isset($_POST['submitID'])){
     $stmt->bindParam(":submit_insta", $_POST['submit_insta']);
     $stmt->bindParam(":submit_discord", $_POST['submit_discord']);
     $stmt->bindParam(":submit_comment", $_POST['submit_comment']);
+    $stmt->bindParam(":userid", $_SESSION['id']);
     $stmt->execute();
     $result = $stmt->fetch();
-    header('Location: submits\submit-add.php'); 
+    header('Location: submits/submit-add.php'); 
 };
 
 
@@ -41,7 +48,7 @@ if(isset($_POST['submitID'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/submit.css">
-    <!-- <link rel="icon" type="image/x-icon" href="img\icon.png">     -->
+    <!-- <link rel="icon" type="image/x-icon" href="img/icon.png">     -->
     <title>016 | Submit</title>
 </head>
 <body>
@@ -52,13 +59,13 @@ if(isset($_POST['submitID'])){
             <p style="margin-left: 35px; ">* = Required</p> <br>
 
             <label>Email*</label>
-            <input class="inp" type="email" name="submit_email" placeholder="Email">
+            <input required class="inp" type="email" name="submit_email" placeholder="Email">
             <label>Artist(s)*</label>
-            <input class="inp" type="text" name="submit_artists" placeholder="Artist(s) (Artist1, Artist2, ...)">
+            <input required class="inp" type="text" name="submit_artists" placeholder="Artist(s) (Artist1, Artist2, ...)">
             <label>Genre*</label>
-            <input class="inp" type="text" name="submit_genre" placeholder="Genre">
+            <input required class="inp" type="text" name="submit_genre" placeholder="Genre">
             <label>Link*</label>
-            <input class="inp" type="text" name="submit_link" placeholder="Link">
+            <input required class="inp" type="text" name="submit_link" placeholder="Link">
             <label class="label-bpm">BPM*</label>
             <select class="inp-select" name="submit_bpm">
                 <option value="128">128</option>
@@ -82,5 +89,10 @@ if(isset($_POST['submitID'])){
         </form>
     </div>
     </div>
+    <?php 
+    
+    // echo $id;
+    
+    ?>
 </body>
 </html>
