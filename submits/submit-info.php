220a0e9c2afa6794be24c8456ad86573e3f112b4
DirkEz <dirk.schaafstra@gmail.com>
2023-06-29 21:43:46 +0200

    Update submit-info.php

--- submits/submit-info.php
+++ submits/submit-info.php
@@ -14,9 +14,9 @@ $id = $_GET['view'];
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="css/style.css">
-   <link href="css\user.css" rel="stylesheet" type="text/css">
-   <link rel="stylesheet" href="css\info-user.css">
-   <link rel="stylesheet" href="css\buttons.css">
+   <link href="css/user.css" rel="stylesheet" type="text/css">
+   <link rel="stylesheet" href="css/info-user.css">
+   <link rel="stylesheet" href="css/buttons.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 <body class="loggedin">
@@ -31,7 +31,7 @@ $id = $_GET['view'];
                 ?>
 				<a href="../user.php"><i class="fa-solid fa-user"></i> User page</a>
 				<a href="user/profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="../user\logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+				<a href="../user/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
